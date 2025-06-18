16ba751753ea19b242eae780cae188b3903b4e17
DiyaBakraniya1 <bakraniyadiya1710@gmail.com>
2025-06-18 23:28:23 +0530

    Module-1 Added

--- Assignments/Module-1/patten5.py
+++ Assignments/Module-1/patten5.py
@@ -1,4 +1,4 @@
-for i in range(5,0,-1): #row
+for i in range(1,6): #row
     for j in range(i): #colume
         print(chr(64+i),end=" ") #64=A 96=a ascii values
     print(" ") #row statement
---
+++ Assignments/Module-1/string.py
@@ -0,0 +1,38 @@
+abc = "this is diya!"
+
+"""print(abc)
+print(abc[0]) #first char.
+print(abc[-1]) #last char.
+print(abc[0:4]) #string slice - range - also count space
+print(len(abc)) """
+
+#String Methods
+print(abc)
+"""print(abc.strip()) #remove extra space
+print(abc.lower())
+print(abc.upper())
+print(abc.replace("Diya","basic python")) 
+
+print(abc.split()) #seprete sentence
+print(abc.capitalize()) #only first letter convert into capital
+print(abc.casefold()) #just like lowercase
+print(abc.count("D")) #count letter 
+
+print(abc.startswith("this")) #starting value boolen
+print(abc.endswith("!")) #ending value boolen
+ 
+
+print(abc.isalpha()) #only alphabets allow
+print(abc.isdigit()) #only numbers allow
+print(abc.isalnum()) #only both alphabets & number allow
+print(abc.islower()) #convert into lowercase
+print(abc.isupper()) #convert into uppercase 
+
+print(abc.title()) #first letter convert into uppercase
+print(abc.istitle()) #if first letter is capital then boolen ans is true otherwise false """
+
+#print(abc.find("diya")) #place number
+print(abc.index("diya"))
+
+
+
